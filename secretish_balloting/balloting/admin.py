f5d96f2ca6472c84c9ce02f9d7b143aa448a4101
Kim van Wyk <kimv@sahomeloans.com>
2021-03-13 21:15:39 +0200

    Adding ballot admin support

--- secretish_balloting/balloting/admin.py
+++ secretish_balloting/balloting/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question, Ballot
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class BallotAdmin(admin.ModelAdmin):
+    list_display = ("name_text", "opening_date", "closing_date", "is_open")
+    fieldsets = [
+        (None, {"fields": ["name_text"]}),
+        (
+            "Date information",
+            {"fields": ["opening_date", "closing_date"], "classes": ["collapse"]},
+        ),
+    ]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Ballot, BallotAdmin)
+admin.site.register(Question, QuestionAdmin)
