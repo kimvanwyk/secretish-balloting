247a8181f2d3b49d531dfff6fefe58a1d7c9b3d5
Kim van Wyk <kimv@sahomeloans.com>
2021-03-14 21:43:04 +0200

    Add .env support

--- secretish_balloting/secretish_balloting/urls.py
+++ secretish_balloting/secretish_balloting/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("", include("balloting.urls")),
     path("admin/", admin.site.urls),
+    path("", include("balloting.urls")),
 ]
