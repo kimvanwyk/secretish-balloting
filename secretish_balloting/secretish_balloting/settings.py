55408cbd949d589befbc390316a8e04c0a5f3b6d
Kim van Wyk <kimv@sahomeloans.com>
2021-04-07 22:36:29 +0200

    Add BASE_URL to ALLOWED_HOSTS

--- secretish_balloting/secretish_balloting/settings.py
+++ secretish_balloting/secretish_balloting/settings.py
@@ -48,7 +48,7 @@ except Exception:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("BASE_URL")]
 
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
