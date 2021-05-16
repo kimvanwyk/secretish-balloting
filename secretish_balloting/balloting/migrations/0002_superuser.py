5d3c7e7967ead3ab24cf109ce23627b5d40f6b81
Kim van Wyk <kimv@sahomeloans.com>
2021-05-16 12:36:17 +0200

    Add readme to pyproject, add superuser migration

---
+++ secretish_balloting/balloting/migrations/0002_superuser.py
@@ -0,0 +1,29 @@
+import os
+
+from django.db import migrations
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("balloting", "0001_initial"),
+    ]
+
+    def generate_superuser(apps, schema_editor):
+        from django.contrib.auth.models import User
+
+        superuser = User.objects.create_superuser(
+            username=os.environ.get("ROOT_USER_USERNAME", "admin"),
+            email=os.environ.get("ROOT_USER_EMAIL", ""),
+            password=os.environ.get("ROOT_USER_PASSWORD"),
+        )
+
+        superuser.save()
+
+    operations = [
+        migrations.RunPython(generate_superuser),
+    ]
